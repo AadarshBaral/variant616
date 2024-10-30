f3bfac7536ffcdb35cad51d526c94583c5c838bc
AadarshBaral <adarsmain101@gmail.com>
2024-10-30 13:54:17 +0545

    fix: fix ssr loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="loading flex justify-center items-center h-[40vh]">
+      loading Post...
+    </div>
+  );
+}
+
+export default loading;
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,46 +3,38 @@ import { Post } from "@/utils/types";
 import Image from "next/image";
 import { Suspense } from "react";
 
-// Server-side function to fetch the post data
-async function getPostData(slug: string): Promise<Post | null> {
-  const post = await fetchPostById(slug);
-  return post;
-}
-
 // Set the cache revalidation time
 export const revalidate = 60; // Revalidate the page
 
 type Params = Promise<{ slug: string }>;
 export default async function Blog(props: { params: Params }) {
   const params = await props.params;
-  const post = await getPostData(params.slug);
+  const post = await fetchPostById(params.slug);
 
   if (!post) {
     return <p>Post not found</p>;
   }
 
   return (
-    <Suspense fallback={<LoadingIndicator />}>
-      <div className="relative h-auto">
-        <div className="flex flex-1 justify-start items-center text-left flex-col w-[90%] sm:w-[70%] mx-auto">
-          <h1 className="text-4xl sm:text-5xl font-bold text-foreground mt-4 self-start pb-10 pt-6 relative">
-            {post.title}
-          </h1>
-          <div className="img-container rounded-xl padding-2">
-            <Image
-              src={post.headerImage}
-              alt={post.title}
-              className="object-cover shadow-inner w-screen h-[35vh] sm:h-[60vh] rounded-xl"
-              width={600}
-              height={600}
-            />
-          </div>
-          <div className="content-container mt-12">
-            <p className="text-white text-lg sm:text-xl">{post.content}</p>
-          </div>
+    <div className="relative h-auto">
+      <div className="flex flex-1 justify-start items-center text-left flex-col w-[90%] sm:w-[70%] mx-auto">
+        <h1 className="text-4xl sm:text-5xl font-bold text-foreground mt-4 self-start pb-10 pt-6 relative">
+          {post.title}
+        </h1>
+        <div className="img-container rounded-xl padding-2">
+          <Image
+            src={post.headerImage}
+            alt={post.title}
+            className="object-cover shadow-inner w-screen h-[35vh] sm:h-[60vh] rounded-xl"
+            width={600}
+            height={600}
+          />
+        </div>
+        <div className="content-container mt-12">
+          <p className="text-white text-lg sm:text-xl">{post.content}</p>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 }
 
