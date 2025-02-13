6cfca38dea010b46e62cee819f4c9cc9c7daec2b
AadarshBaral <adarsmain101@gmail.com>
2025-02-13 23:41:29 +0545

    appwrite changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cloud.appwrite.io"],
   },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
